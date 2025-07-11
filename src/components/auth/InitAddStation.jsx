7c81e261cda5d1bf3b346355b6b00dfefd00801c
Vishwani Vilochana <vishwani2002@gmail.com>
2025-07-11 22:32:05 +0530

    updated Addstation page

--- src/components/auth/InitAddStation.jsx
+++ src/components/auth/InitAddStation.jsx
@@ -5,7 +5,7 @@ import { COLORS, FONTS } from '../../constants'
 import Button from '../ui/Button';
 import InputField from '../ui/InputField';
 import { FiEdit, FiMessageCircle } from 'react-icons/fi';
-
+import stationImage from '../../assets/cuate.svg'
 
 
 const elecProviders = [
@@ -100,19 +100,23 @@ export default function LandingPage() {
 
     return (
         <Layout>
-        <div className="text-center py-20">
+        <div className="text-center py-8">
+            <div className="flex w-full justify-center">
+                <img src={stationImage} alt="Dashboard" className="w-auto h-72 object-cover rounded-lg" />
+            </div>
+            
             <h1 
-            className="mb-2"
+            className="mb-0"
             style={{
                 font: FONTS.weights.normal,
                 fontSize: FONTS.sizes['4xl'],
                 color: COLORS.mainTextColor
             }}
             >
-            Hello Andrew!
+            Hello Adeesha!
             </h1>
             <p 
-            className="max-w-md mx-auto md:max-w-3xl mb-10"
+            className="max-w-md mx-auto md:max-w-3xl mb-12"
             style={{
                 font: FONTS.weights.normal,
                 fontSize: FONTS.sizes.base,
@@ -121,10 +125,20 @@ export default function LandingPage() {
             >
             Now you can add your charging station.
             </p>
-            <div className="flex justify-center space-x-4">
+            <div className="flex-column justify-center items-center">
                 <Button variant="primary" type="button" onClick={ () => setShowForm(true)}>
-                    Add Station
+                    Add Charging Station
                 </Button>
+                <p 
+                className="mt-4 underline" 
+                style={{ 
+                    font: FONTS.weights.normal,
+                    fontSize: FONTS.sizes.xs,
+                    color: COLORS.secondaryText,
+                }}
+                >
+                Installation Process & Charges
+                </p>
             </div>
             {/* Chat Icon */}
             <button className="fixed flex gap-1 bottom-6 right-6 bg-white shadow-lg p-4 rounded-full" style={{ backgroundColor: COLORS.primary}}>
@@ -445,7 +459,7 @@ export default function LandingPage() {
                                 <Button variant="outline" onClick={() => setCurrentStep(1)}>
                                     Back
                                 </Button>
-                                <Button variant="primary">Submit</Button>
+                                <Button variant="primary">Add Station</Button>
                             </div>
                         </div>
                         )}
